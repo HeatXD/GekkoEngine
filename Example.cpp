5d56bd845bc952702670817bd3f6a55852068a06
Jamie Meyer <jamiexxl5@hotmail.com>
2025-01-29 03:01:34 +0100

    this behaviour system works pretty well.

--- Example.cpp
+++ Example.cpp
@@ -4,11 +4,13 @@ int main() {
     Engine en;
 
     en.CreateCharacterInstance("ExampleCharacter", 0);
-    en.CreateCharacterInstance("ExampleCharacter", 0);
 
     std::cout << "Character Count: " << en.NumRegisteredCharacters() << "\n";
     // Simulate updates
     for (int i = 0; i < 100; ++i) {
+        if (i == 5) {
+            en.CreateCharacterInstance("ExampleCharacter", 0);
+        }
         std::cout << "Frame " << i << ":\n";
         en.Update();
         std::cout << "-----------------\n";
